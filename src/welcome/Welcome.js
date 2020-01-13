0af4d7b6b03d6eb42e1182c646f739e2cda0c4a4
DataCollectApp <datacollectapp@jarand.dev>
2020-01-13 21:06:15 +0100

    Added some counters

--- src/welcome/Welcome.js
+++ src/welcome/Welcome.js
@@ -3,19 +3,38 @@ import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 
 class Welcome extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      twitchUserCount: -1,
+      nameChangeCount: -1
+    }
+  }
+
+  componentDidMount() {
+    fetch('/twitch-data-api/v1/statistics/counters')
+    .then(response => response.json())
+    .then(data => this.setState({twitchUserCount: data.twitchUserCount, nameChangeCount: data.nameChangeCount}));
+  }
+
   render() {
     return (
-        <Container>
-          <Paper>
-            <h1>Welcome to Twitch Data</h1>
-            <p>
-              Twitch Data is a small project created to collect data (chat messages and punishments) from Norwegian Twitch channels.<br/>
-              The data collector has been running since 10/01/2020 and the goal is to have it running 24/7.<br/>
-              You can not see chat messages at the moment, but it's saved and I might share it later.<br/>
-              I am working on more features (user search etc.) for the website.
-            </p>
-          </Paper>
-        </Container>
+        <div>
+          <Container>
+            <Paper>
+              <h1>Welcome to Twitch Data</h1>
+              <p>
+                Twitch Data is a small project created to collect data (chat messages and punishments) from Norwegian Twitch channels.<br/>
+                The data collector has been running since 10/01/2020 and the goal is to have it running 24/7.<br/>
+                You can not see chat messages at the moment, but it's saved and I might share it later.<br/>
+                I am working on more features (user search etc.) for the website.<br/><br/>
+                <b>{this.state.twitchUserCount}</b> twitch users and <b>{this.state.nameChangeCount}</b> name changes has been collected.
+              </p>
+            </Paper>
+          </Container>
+        </div>
     );
   }
 }
