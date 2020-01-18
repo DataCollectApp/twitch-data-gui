9903720fdd6ac1dd2b48d367e524fbc88f3205c0
DataCollectApp <datacollectapp@jarand.dev>
2020-01-18 09:43:39 +0100

    Wait before loading

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -8,29 +8,35 @@ class Profile extends React.Component {
 
   constructor(props) {
     super(props);
+  }
 
+  componentDidMount() {
     fetch('/twitch-data-api/v1/twitch-user/username?username=' + this.props.match.params.username)
     .then(response => response.json())
-    .then(data => this.state = {user: data});
+    .then(data => this.setState({user: data}));
   }
 
   render() {
-    return (
-        <div>
-          <h1>{this.state.user.displayName}</h1>
-          <Grid container spacing={1}>
-            <Grid item sm={12} md={6} lg={6} xl={6}>
-              <RecentNameChange user={this.state.user}/>
+    if (this.state.user) {
+      return (
+          <div>
+            <h1>{this.state.user.displayName}</h1>
+            <Grid container spacing={1}>
+              <Grid item sm={12} md={6} lg={6} xl={6}>
+                <RecentNameChange user={this.state.user}/>
+              </Grid>
+              <Grid item sm={12} md={6} lg={6} xl={6}>
+                <RecentClearChat user={this.state.user}/>
+              </Grid>
+              <Grid item sm={12} md={12} lg={12} xl={12}>
+                <RecentClearMessage user={this.state.user}/>
+              </Grid>
             </Grid>
-            <Grid item sm={12} md={6} lg={6} xl={6}>
-              <RecentClearChat user={this.state.user}/>
-            </Grid>
-            <Grid item sm={12} md={12} lg={12} xl={12}>
-              <RecentClearMessage user={this.state.user}/>
-            </Grid>
-          </Grid>
-        </div>
-    );
+          </div>
+      );
+    } else {
+      return (<div><h1>Loading...</h1></div>);
+    }
   }
 }
 
